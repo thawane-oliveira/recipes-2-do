a8ba4456ac3bcc0cd74b47f7d9d35fe57d0090e9
DIogo Alpino <diogoalpino@gmail.com>
2022-11-30 16:14:23 -0300

    Testes do Requisito 12

---
+++ src/tests/SearchBar.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import AppProvider from '../context/AppProvider';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testes do SearchBar', () => {
+  it('Verifica se ', async () => {
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <App />
+      </AppProvider>,
+    );
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginButton = screen.getByTestId('login-submit-btn');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    userEvent.type(emailInput, 'gappy@soft.wet');
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(loginButton);
+
+    const searchButton = await screen.findByTestId('search-button');
+    userEvent.click(searchButton);
+
+    const searchInput = await screen.findByTestId('search-input');
+    expect(searchInput).toBeVisible();
+    userEvent.type(searchInput, 'chicken');
+
+    const ingredientRadio = await screen.findByTestId('ingredient-search-radio');
+    expect(ingredientRadio).toBeInTheDocument();
+    userEvent.click(ingredientRadio);
+  });
+});
