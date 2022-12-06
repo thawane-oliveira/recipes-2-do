91a27db8313b659e36d16c8288a2858e7cb2555a
José Wemerson - turma 25 - tribo A <josepdrjw@gmail.com>
2022-12-06 18:29:38 -0300

    Desenvolve requisito 50, 51 e 52 sem corbetura de 45% dos teste

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Header from '../components/Header';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
 
 function FavoriteRecipes() {
   const responseFav = JSON.parse(localStorage.getItem('favoriteRecipes'));
@@ -49,6 +51,79 @@ function FavoriteRecipes() {
         Drinks
 
       </button>
+      {
+        fav
+          .map((element, index) => {
+            if (element.type === 'meal') {
+              return (
+                <div key={ index }>
+                  <img
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ element.image }
+                    alt={ element.name }
+                  />
+                  <p data-testid={ `${index}-horizontal-top-text` }>
+                    { `${element.nationality} - ${element.category}` }
+                  </p>
+
+                  <p data-testid={ `${index}-horizontal-name` }>{ element.name}</p>
+
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-favorite-btn` }
+                    onClick={ () => {} }
+                    src="blackHeartIcon"
+                  >
+                    <img src={ blackHeartIcon } alt={ element.name } />
+                  </button>
+
+                  <button
+                    type="button"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    onClick={ () => {} }
+                    src="shareIcon"
+                  >
+                    <img src={ shareIcon } alt={ element.name } />
+                  </button>
+
+                </div>
+              );
+            }
+            return (
+              <div key={ index }>
+                <img
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ element.image }
+                  alt={ element.name }
+                />
+
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  { element.alcoholicOrNot }
+                </p>
+
+                <p data-testid={ `${index}-horizontal-name` }>{ element.name}</p>
+
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  onClick={ () => {} }
+                  src="blackHeartIcon"
+                >
+                  <img src={ blackHeartIcon } alt={ element.name } />
+                </button>
+
+                <button
+                  type="button"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  onClick={ () => {} }
+                  src="shareIcon"
+                >
+                  <img src={ shareIcon } alt={ element.name } />
+                </button>
+              </div>
+            );
+          })
+      }
     </div>
   );
 }
