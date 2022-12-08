c0be6b1da5b62f199c99e1010524694f0456f454
Fernanda Guerra <fernandaguerra.fag@gmail.com>
2022-12-08 19:17:19 -0300

    Adiciona estilização da página Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import './styles/Login.css';
 
 function Login() {
   const { email, setPassword, setEmail,
@@ -16,32 +17,48 @@ function Login() {
     history.push('/meals');
   };
   return (
-    <form>
-      <input
-        data-testid="email-input"
-        placeholder="Email"
-        onChange={ ({ target }) => {
-          setEmail(target.value);
-        } }
-      />
-      <input
-        data-testid="password-input"
-        placeholder="Password"
-        type="password"
-        onChange={ ({ target }) => {
-          setPassword(target.value);
-        } }
-      />
-      <button
-        type="button"
-        data-testid="login-submit-btn"
-        disabled={ isDisabled }
-        onClick={ () => handleSubmit(email) }
-      >
-        Enter
+    <div className="teste">
 
-      </button>
-    </form>
+      <form className="loginContainer">
+        <img src="https://imageup.me/images/recipes-app.png" alt="ícone da tela de login" className="login-img" />
+
+        <h1 className="loginTitle"> Recipes App </h1>
+        <input
+          className="email"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ ({ target }) => {
+            setEmail(target.value);
+          } }
+        />
+        <input
+          className="password"
+          data-testid="password-input"
+          placeholder="Password"
+          type="password"
+          onChange={ ({ target }) => {
+            setPassword(target.value);
+          } }
+        />
+        <button
+          className={ isDisabled ? 'submit-disabled' : 'submit-button' }
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ isDisabled }
+          onClick={ () => handleSubmit(email) }
+        >
+          Enter
+
+        </button>
+
+        <footer className="loginFooter">
+          <b> &copy; Group-2A O melhor do Brasil! </b>
+        </footer>
+
+      </form>
+
+    </div>
+
   );
 }
 
