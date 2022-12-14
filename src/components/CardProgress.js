8b6bfe821f8c46cec6c4590aca819ddbbcd3b976
Thawane <thawaneapoliveira@gmail.com>
2022-12-14 13:51:59 -0300

    Botão de retornar para tela de detalhes

--- src/components/CardProgress.js
+++ src/components/CardProgress.js
@@ -1,10 +1,30 @@
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import './styles/RecipeInProgress.css';
+import returnImg from '../images/return.png';
 
 function CardProgress({
   title, photo, category, instructions, ing, shareBtn, favBtn, finishBtn }) {
+  const history = useHistory();
+  const returnPrevious = () => {
+    const local = history.location.pathname;
+    const splitedId = local.split('/')[2];
+    if (local.includes('drinks')) return history.push(`/drinks/${splitedId}`);
+    return history.push(`/meals/${splitedId}`);
+  };
   return (
     <div className="progress-container">
+      <button
+        type="button"
+        className="return-button"
+        onClick={ returnPrevious }
+      >
+        <img
+          src={ returnImg }
+          alt="return-button"
+          className="return-img"
+        />
+      </button>
       <h3 className="progress-title" data-testid="recipe-title">{title}</h3>
       <h4 data-testid="recipe-category" className="progress-category">
         {category}
